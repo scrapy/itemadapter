c9106842cb1c8f4de9d94bc881fa7f000539e4ab
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2020-04-28 08:21:12 -0300

    Bump version 0.0.3 -> 0.0.4

--- itemadapter/__init__.py
+++ itemadapter/__init__.py
@@ -2,4 +2,4 @@ from .adapter import ItemAdapter  # noqa: F401
 from .utils import is_item  # noqa: F401
 
 
-__version__ = "0.0.3"
+__version__ = "0.0.4"
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="itemadapter",
-    version="0.0.3",
+    version="0.0.4",
     license="BSD",
     description="Common interface for Scrapy items",
     long_description=long_description,
