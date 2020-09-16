9de20fcda3e69de67fa7fedd91d1086d115b028f
Adrián Chaves <adrian@chaves.io>
2020-09-16 19:59:44 +0200

    Sort imports

--- itemadapter/__init__.py
+++ itemadapter/__init__.py
@@ -1,5 +1,5 @@
 from .adapter import ItemAdapter  # noqa: F401
-from .utils import is_item, get_field_meta_from_class  # noqa: F401
+from .utils import get_field_meta_from_class, is_item  # noqa: F401
 
 
 __version__ = "0.1.0"
