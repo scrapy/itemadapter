e3083b3f0c6de47488b30e78f6eb80be7dde45ca
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2020-09-28 11:48:41 -0300

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="itemadapter",
-    version="0.1.0",
+    version="0.1.1",
     license="BSD",
     description="Common interface for data container classes",
     long_description=long_description,
