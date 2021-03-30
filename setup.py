3c32d5ad56a449533b1c1299f295420fc13a6cf7
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2021-03-30 00:09:00 -0300

    Add py39 to CI and setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Framework :: Scrapy",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP",
