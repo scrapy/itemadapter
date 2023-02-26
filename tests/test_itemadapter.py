2732297ec28ee768b03d3ed27bf410d18649284f
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2023-02-26 13:34:13 -0300

    Add tests

--- tests/test_itemadapter.py
+++ tests/test_itemadapter.py
@@ -1,10 +1,13 @@
 import unittest
+from collections import deque
 
-from itemadapter.adapter import ItemAdapter
+from itemadapter.adapter import ItemAdapter, DictAdapter
+
+from tests import DataClassItem
 
 
-class SubclassedItemAdapter(ItemAdapter):
-    pass
+class DictOnlyItemAdapter(ItemAdapter):
+    ADAPTER_CLASSES = deque([DictAdapter])
 
 
 class ItemAdapterTestCase(unittest.TestCase):
@@ -13,5 +16,23 @@ class ItemAdapterTestCase(unittest.TestCase):
         self.assertEqual(repr(adapter), "<ItemAdapter for dict(foo='bar')>")
 
     def test_repr_subclass(self):
-        adapter = SubclassedItemAdapter(dict(foo="bar"))
-        self.assertEqual(repr(adapter), "<SubclassedItemAdapter for dict(foo='bar')>")
+        adapter = DictOnlyItemAdapter(dict(foo="bar"))
+        self.assertEqual(repr(adapter), "<DictOnlyItemAdapter for dict(foo='bar')>")
+
+    def test_as_dict_subclass(self):
+        """'asdict' method of ItemAdapter subclasses handles items even
+        if they're not handled by the subclass itself.
+        """
+        item = dict(
+            foo="bar",
+            dataclass_item=DataClassItem(name="asdf", value="qwerty"),
+            itemadapter=ItemAdapter({"a": 1, "b": 2}),
+        )
+        self.assertEqual(
+            DictOnlyItemAdapter(item).asdict(),
+            dict(
+                foo="bar",
+                dataclass_item=dict(name="asdf", value="qwerty"),
+                itemadapter={"a": 1, "b": 2},
+            ),
+        )
