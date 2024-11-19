3088cc4860b26b7dab74a5a9cd7c25ed3f83bcd2
Andrey Rakhmatullin <wrar@wrar.name>
2024-11-19 14:14:50 +0500

    Fix a typing issue with typed scrapy, bump mypy. (#88)

--- itemadapter/_imports.py
+++ itemadapter/_imports.py
@@ -6,12 +6,16 @@ _scrapy_item_classes: tuple
 try:
     import scrapy  # pylint: disable=W0611 (unused-import)
 except ImportError:
-    scrapy = None  # type: ignore [assignment]
+    scrapy = None  # type: ignore[assignment]
     _scrapy_item_classes = ()
 else:
     try:
         # handle deprecated base classes
-        _base_item_cls = getattr(scrapy.item, "_BaseItem", scrapy.item.BaseItem)
+        _base_item_cls = getattr(
+            scrapy.item,
+            "_BaseItem",
+            scrapy.item.BaseItem,  # type: ignore[attr-defined]
+        )
     except AttributeError:
         _scrapy_item_classes = (scrapy.item.Item,)
     else:
@@ -20,9 +24,9 @@ else:
 try:
     import attr  # pylint: disable=W0611 (unused-import)
 except ImportError:
-    attr = None  # type: ignore [assignment]
+    attr = None  # type: ignore[assignment]
 
 try:
     import pydantic  # pylint: disable=W0611 (unused-import)
 except ImportError:
-    pydantic = None  # type: ignore [assignment]
+    pydantic = None  # type: ignore[assignment]
